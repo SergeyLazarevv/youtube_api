aa4aba928bef696bc9117b991e5d9b6dadde725d
Sergey <serg_lazarev13@mail.ru>
2020-03-23 23:31:20 +0400

    23.03.20

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router} from 'react-router-dom'
+import { Router } from 'react-router-dom'
 import AsideMenu from './Components/Aside_menu/Aside_menu'
 import HiddenMenu from './Components/HiddenMenu/HiddenMenu'
 import Content from './Content'
